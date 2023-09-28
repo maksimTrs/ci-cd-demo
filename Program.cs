625caecf9d070046d973161eee3b92f74d23d337
aussiearef <arefkr@gmail.com>
2023-09-28 13:42:30 +1000

    Simplified the api to return Hello

--- Program.cs
+++ Program.cs
@@ -1,28 +1,12 @@
 using System.Net;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(b =>
-    {
-        b.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-    });
-});
-
 var app = builder.Build();
-app.UseCors();
 
-app.MapGet("/search",  () =>
+app.MapGet("/", () =>
 {
-    return new HttpResponseMessage(HttpStatusCode.OK){
-        Content = new StringContent("Hello!")
-    };
+  return "Hello";
 });
-
-
 
 app.Run();
